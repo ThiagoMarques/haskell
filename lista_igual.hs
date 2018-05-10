b5f6a8db6e630431ae07fcd4ebc4cbd1ab4f838b
ThiagoMarques <thiagossmarques@gmail.com>
2018-05-10 11:07:19 -0300

    Update lista_igual.hs

--- lista_igual.hs
+++ lista_igual.hs
@@ -1,6 +1,6 @@
 lista_igual :: [Int] -> [Int] -> Bool
 lista_igual [] [] = True
-lista_igual [] __ = False
+lista_igual [] _ = False
 lista_igual _ [] = False
 lista_igual (x:xs) (y:ys) | (x == y) = lista_igual xs ys
 			  | otherwise = False
