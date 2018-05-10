acf846b22329802e68273a3bbb8d0dc0267cc2a7
Thiago Marques <thiagossmarques@gmail.com>
2018-05-09 21:24:24 -0300

    Tamanho de uma lista

---
+++ inter.hs
@@ -0,0 +1,5 @@
+inter l1 l2 = [e | e <- l1, member e l2]
+inter [] l2 = []
+inter (a:as)l2 
+	    | member a l2 = a :: inter as l2
+	    | otherwise = inter as l2
---
+++ size_list.hs
@@ -0,0 +1,3 @@
+size_list :: [Int] -> Int
+size_list [] = 0
+size_list (x:xs) = 1 + size_list xs
