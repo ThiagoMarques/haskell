16f1ac0584055cd239bd1261e327c961cbef836b
Thiago Marques <thiagossmarques@gmail.com>
2018-05-13 22:37:06 -0300

    Diferença entre duas listas

--- 1_a.hs
+++ 1_a.hs
@@ -1,7 +1,3 @@
-aux :: Int-> Int -> [Int]
-aux 0 = []
-
-dif :: [Int] -> [Int] -> Int
-dif [][] = aux 0 0
-dif (x:xs)(y:ys) | (x == y) = dif xs ys
-		 | otherwise = aux x y
+dif :: [Int] -> [Int] -> [Int]
+dif [][] = []
+dif (x:xs)(y:ys) = x-y : dif xs ys
