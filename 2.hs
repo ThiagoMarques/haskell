b5b53a1013e1cad9d179eed9879cb0132cda3d1d
Thiago Marques <thiagossmarques@gmail.com>
2018-05-15 09:41:56 -0300

    Alterando a função Expr e o tipo eval para multiplicações

---
+++ 2.hs
@@ -0,0 +1,10 @@
+data Expr = Lit Int 
+			| Add Expr Expr
+			| Sub Expr Expr
+			| Mul Expr Expr
+
+eval :: Expr -> Int
+eval (Lit n) = n
+eval (Add e1 e2) = (eval e1) + (eval e2)
+eval (Sub e1 e2) = (eval e1) - (eval e2)
+eval (Mul e1 e2) = (eval e1) * (eval e2)
